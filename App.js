9c4461a8dac0ffd5efaa7248efefa24898ceae59
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-02 05:50:49 -0700

    Commiting a new project

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { LogBox } from 'react-native';
+import {
+  NavigationContainer,
+  navigationContainer,
+} from '@react-navigation/native';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './Redux/store';
+
+// Navigators
+import Main from './Navigators/Main';
+
+//Screens
+import Header from './Shared/Header';
+import ProductContainer from './Screens/Products/ProductContainer';
+
+LogBox.ignoreAllLogs(true);
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Header />
+        <Main />
+      </NavigationContainer>
+    </Provider>
+  );
+}
