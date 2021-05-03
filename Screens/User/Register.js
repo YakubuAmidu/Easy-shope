bbf01370fd590a50a87f28ce53b0b378a78259ee
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-03 01:25:59 -0700

    Updating changes

--- Screens/User/Register.js
+++ Screens/User/Register.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import FormContainer from '../../Shared/Form/FormContainer';
+import Input from '../../Shared/Form/Input';
+import Error from '../../Shared/Error';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 const Register = (props) => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
   return (
-    <View>
-      <Text>Register Screen</Text>
-    </View>
+    <KeyboardAwareScrollView title={'Register'}>
+      <Input
+        placeholder={'Email'}
+        name={'email'}
+        id={'email'}
+        onChangeText={(text) => setEmail(text)}
+      />
+    </KeyboardAwareScrollView>
   );
 };
 
