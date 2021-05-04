bf64fe17a2cec29ced598691bd4a7d96b332b6f5
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-03 20:38:55 -0700

    Updating changes

--- Context/store/Auth.js
+++ Context/store/Auth.js
@@ -5,3 +5,22 @@ import AsyncStorage from '@react-native-community/async-storage';
 import AuthReducer from '../reducers/Authreducer';
 import { setCurrentUser } from '../actions/Authactions';
 import AuthGlobal from './Authactions';
+
+const Auth = (props) => {
+  const [stateUser, dispatch] = useReducer(AuthReducer, {
+    isAuthenticated: null,
+    user: {},
+  });
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+    if (AsyncStorage.jwt) {
+      const decoded = AsyncStorage.jwt ? AsyncStorage.jwt : '';
+      if (setShowChild) {
+        dispatch(setCurrentUser(jwt_decoded(decoded)));
+      }
+    }
+    return () => setShowChild(false);
+  }, []);
+};
