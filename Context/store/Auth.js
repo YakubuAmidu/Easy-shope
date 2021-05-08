56f45f1cdff9a8b1455331f34788b088cb37d8d6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-08 04:00:20 -0700

    Updating changes

--- Context/store/Auth.js
+++ Context/store/Auth.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useReducer } from 'react-native';
-import jwt_decoded from 'jwt-decode';
-import AsyncStorage from '@react-native-community/async-storage';
+import React, { useEffect, useReducer, userEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import AuthReducer from '../reducers/Authreducer';
-import { setCurrentUser } from '../actions/Authactions';
-import AuthGlobal from './Authactions';
+import authReducer from '../reducers/Auth.reducer';
+import { setCurrentUser } from '../actions/Auth.actions';
+import AuthGlobal from './AuthGlobal';
 
 const Auth = (props) => {
-  const [stateUser, dispatch] = useReducer(AuthReducer, {
+  const [stateUser, dispatch] = useReducer(authReducer, {
     isAuthenticated: null,
     user: {},
   });
@@ -18,9 +18,26 @@ const Auth = (props) => {
     if (AsyncStorage.jwt) {
       const decoded = AsyncStorage.jwt ? AsyncStorage.jwt : '';
       if (setShowChild) {
-        dispatch(setCurrentUser(jwt_decoded(decoded)));
+        dispatch(setCurrentUserUser(jwt_decode(decoded)));
       }
     }
     return () => setShowChild(false);
   }, []);
+
+  if (!showChild) {
+    return null;
+  } else {
+    return (
+      <AuthGlobal.Provider
+        value={{
+          stateUser,
+          dispatch,
+        }}
+      >
+        {props.children}
+      </AuthGlobal.Provider>
+    );
+  }
 };
+
+export default Auth;
