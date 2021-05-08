6ceb365408ec01aca6d5b3dc4854ed81602af991
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-08 05:48:11 -0700

    Updating changes

---
+++ Screens/Admin/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Orders = (props) => {
+  return (
+    <View>
+      <Text>Order</Text>
+    </View>
+  );
+};
+
+export default Orders;
