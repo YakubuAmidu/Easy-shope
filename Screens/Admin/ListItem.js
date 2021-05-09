3eac694f91f8eaba2b4e374648b6e6cbf3959fd6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-09 01:42:21 -0700

    Updating changes

---
+++ Screens/Admin/ListItem.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableHighLight,
+  TouchableOpacity,
+  Dimensions,
+  Button,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+var { width } = Dimensions.get('window');
+
+const ListItem = (props) => {
+  return (
+    <View>
+      <TouchableOpacity></TouchableOpacity>
+    </View>
+  );
+};
+
+export default ListItem;
