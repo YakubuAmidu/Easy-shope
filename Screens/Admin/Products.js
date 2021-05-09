688ee51fa8f05e2b00735119d6de5ff17ed93feb
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-09 01:16:08 -0700

    Updating changes

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -16,7 +16,36 @@ import axios from 'axios';
 import baseURL from '../../assets/common/baseUrl';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+var { height, width } = Dimensions.get('window');
+
 const Products = (props) => {
+  const [product, setProduct] = useState();
+  const [productFilter, setProductFilter] = useState();
+  const [loading, setLoading] = useState(true);
+  const [token, setToken] = useState();
+
+  useFocusEffect(
+    useCallback(() => {
+      //Get Token
+      AsyncStorage.getItem('jwt')
+        .then((res) => {
+          setToken(res);
+        })
+        .catch((error) => console.log(error));
+
+      axios.get(`${baseURL}products`).then((res) => {
+        setProductList(res.data);
+        setProductFilter(res.data);
+        setLoading(false);
+      });
+      return () => {
+        setProductList();
+        setProductFilter();
+        setLoading(true);
+      };
+    }, [])
+  );
+
   return (
     <View>
       <Text>Products</Text>
