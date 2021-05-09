36c12743d0e4dcc93c4e9626c6329f677a586374
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-09 00:56:37 -0700

    Updating changes

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -1,5 +1,20 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  Dimensions,
+  Button,
+} from 'react-native';
+import { Header, Item, Input } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useFocusEffect } from '@react-navigation/native';
+
+import axios from 'axios';
+import baseURL from '../../assets/common/baseUrl';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Products = (props) => {
   return (
