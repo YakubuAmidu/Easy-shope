b199942a3c2c862c2fe5627cc4379efd7ce690be
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-09 02:22:55 -0700

    Updating changes

--- Screens/Admin/ListItem.js
+++ Screens/Admin/ListItem.js
@@ -16,7 +16,26 @@ var { width } = Dimensions.get('window');
 const ListItem = (props) => {
   return (
     <View>
-      <TouchableOpacity></TouchableOpacity>
+      <TouchableOpacity
+      //onPress
+      >
+        <Image
+          source={{
+            uri: props.image
+              ? props.image
+              : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
+          }}
+          rensizeMode="contain"
+        />
+        <Text>{props.brand}</Text>
+        <Text numberOfLines={1} ellipsizeMode="tail">
+          {props.name}
+        </Text>
+        <Text numberOfLines={1} ellipsizeMode="tail">
+          {props.category.name}
+        </Text>
+        <Text>$ {props.price}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
