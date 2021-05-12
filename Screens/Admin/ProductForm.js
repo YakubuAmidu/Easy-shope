c20c1bff9d617a6799b0f6c604809426a92cb6bc
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-11 23:51:00 -0700

    Updating changes

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -1,5 +1,20 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import { Item, Picker } from 'native-base';
+import FormContainer from '../../Shared/Form/FormContainer';
+import Input from '../../Shared/Form/Input';
+import EasyButton from '../../Shared/StyledComponents/EasyButton';
+import Error from '../../Shared/Error';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ProductForm = (props) => {
   return (
