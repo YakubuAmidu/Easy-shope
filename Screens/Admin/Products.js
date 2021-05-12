3f36d9d24ab6aaab4f495a2827aaf04aa3204ac9
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-11 19:25:01 -0700

    Updating changes

--- Screens/Admin/Products.js
+++ Screens/Admin/Products.js
@@ -78,6 +78,18 @@ const Products = (props) => {
     );
   };
 
+  const deleteProduct = (id) => {
+    axios
+      .delete(`${baseURL}products/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        const products = productFilter.filter((item) => item.id !== id);
+        setProductFilter(products);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <View>
       <View>
@@ -100,7 +112,12 @@ const Products = (props) => {
           data={productFilter}
           listHeaderComponent={ListHeader}
           renderItem={({ item, index }) => (
-            <ListItem {...item} navigation={props.navigation} index={index} />
+            <ListItem
+              {...item}
+              navigation={props.navigation}
+              index={index}
+              delete={deleteProduct}
+            />
           )}
           keyExtractor={(item) => item.id}
         />
