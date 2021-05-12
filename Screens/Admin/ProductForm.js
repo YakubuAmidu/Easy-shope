7cd34ce1f4c2562408691c8694bc50af630d63e1
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-12 00:01:57 -0700

    Updating changes

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -15,8 +15,25 @@ import Error from '../../Shared/Error';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Toast from 'react-native-toast-message';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import baseURL from '../../assets/common/baseUrl';
+import axios from 'axios';
 
 const ProductForm = (props) => {
+  const [pickerValue, setPickerValue] = useState();
+  const [brand, setBrand] = useState();
+  const [name, setName] = useState();
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState();
+  const [image, setImage] = useState();
+  const [mainImage, setMainImage] = useState();
+  const [category, setCategory] = useState();
+  const [categories, setCategories] = useState([]);
+  const [token, setToken] = useState();
+  const [error, setError] = useState();
+  const [countInStock, setCountInStock] = useState();
+  const [rating, setRating] = useState();
+  const [isFeatured, setIsFeatured] = useState(false);
+
   return (
     <View>
       <Text>ProductForm</Text>
