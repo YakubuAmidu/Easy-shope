47b4c8c994b23877b63181e08c97111f0adf11c8
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-12 04:13:02 -0700

    Initial changes

--- Screens/Admin/Categories.js
+++ Screens/Admin/Categories.js
@@ -1,5 +1,18 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  TextInput,
+} from "react-native";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
+import baseURL from "../../Shared/StyledComponents/EasyButton";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+var { width } = Dimensions.get("window");
 
 const Categories = (props) => {
   return (
