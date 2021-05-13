493f615c7184691015aa140b285af1559f702e36
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-13 13:22:37 -0700

    Updating changes

--- Shared/OrderCard.js
+++ Shared/OrderCard.js
@@ -11,6 +11,28 @@ import axios from "axios";
 import baseURL from "../assets/common/baseUrl";
 
 const OrderCard = (props) => {
+  const [orderStatus, setOrderStatus] = useState();
+  const [statusText, setStatusText] = useState();
+  const [statusChange, setStatusChange] = useState();
+  const [token, setToken] = useState();
+  const [cardColor, setCardColor] = useState();
+
+  useEffect(() => {
+    if (props.status == "3") {
+      setOrderStatus(<TrakfickLight unavailable></TrakfickLight>);
+      setStatusText("Pending");
+      setCardColor("#E74C3C");
+    } else if (props.status == "2") {
+      setOrderStatus(<TraficLight limited></TraficLight>);
+      setStatusText("Shipped");
+      setCardColor("#F1C40F");
+    } else {
+      setOrderStatus();
+      setStatusText();
+      setCardColor();
+    }
+  }, []);
+
   return <View></View>;
 };
 export default OrderCard;
