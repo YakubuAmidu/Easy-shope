f47c88296d0b21f14088d6cf3ba31bc0b98a0109
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-05-14 00:29:04 -0700

    Initial commit

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,33 +1,21 @@
-import React from 'react';
-import {
-  View,
-  Dimensions,
-  Button,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import {
-  Container,
-  Text,
-  Left,
-  Right,
-  H1,
-  ListItem,
-  Thumbnail,
-  Body,
-} from 'native-base';
-import { SwipeListView } from 'react-native-swipe-list-view';
-import CartItem from './CartItem';
+import React, { useContext } from "react";
+import { View, Dimensions, StyleSheet, TouchableOpacity } from "react-native";
+import { Container, Text, Left, Right, H1 } from "native-base";
+import { SwipeListView } from "react-native-swipe-list-view";
+import CartItem from "./CartItem";
 
-import Icon from 'react-native-vector-icons/FontAwesome';
-import EasyButton from '../../Shared/StyledComponents/EasyButton';
+import Icon from "react-native-vector-icons/FontAwesome";
+import EasyButton from "../../Shared/StyledComponents/EasyButton";
 
-import { connect } from 'react-redux';
-import * as actions from '../../Redux/Actions/cartActions';
+import { connect } from "react-redux";
+import * as actions from "../../Redux/Actions/cartActions";
+import AuthGlobal from "../../Context/store/AuthGlobal";
 
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 const Cart = (props) => {
+  const context = useContext(AuthGlobal);
+
   var total = 0;
   props.cartItems.forEach((cart) => {
     return (total += cart.product.price);
@@ -37,7 +25,7 @@ const Cart = (props) => {
     <>
       {props.cartItems.length ? (
         <Container>
-          <H1 style={{ alignSelf: 'center' }}>Cart</H1>
+          <H1 style={{ alignSelf: "center" }}>Cart</H1>
           <SwipeListView
             data={props.cartItems}
             renderItem={(data) => <CartItem item={data} />}
@@ -47,7 +35,7 @@ const Cart = (props) => {
                   style={styles.hiddenButton}
                   onPress={() => props.removeFromCart(data.item)}
                 >
-                  <Icon name="trash" color={'white'} size={30} />
+                  <Icon name="trash" color={"white"} size={30} />
                 </TouchableOpacity>
               </View>
             )}
@@ -65,17 +53,27 @@ const Cart = (props) => {
             </Left>
             <Right>
               <EasyButton danger medium onPress={() => props.clearCart()}>
-                <Text style={{ color: '#fff' }}>Clear</Text>
+                <Text style={{ color: "#fff" }}>Clear</Text>
               </EasyButton>
             </Right>
             <Right>
-              <EasyButton
-                primary
-                medium
-                onPress={() => props.navigation.navigate('Checkout')}
-              >
-                <Text style={{ color: '#fff' }}>Checkout</Text>
-              </EasyButton>
+              {context.stateUser.isAuthenticated ? (
+                <EasyButton
+                  primary
+                  medium
+                  onPress={() => props.navigation.navigate("Checkout")}
+                >
+                  <Text style={{ color: "#fff" }}>Checkout</Text>
+                </EasyButton>
+              ) : (
+                <EasyButton
+                  secondary
+                  medium
+                  onPress={() => props.navigation.navigate("Login")}
+                >
+                  <Text style={{ color: "#fff" }}>Login</Text>
+                </EasyButton>
+              )}
             </Right>
           </View>
         </Container>
@@ -106,31 +104,31 @@ const mapDispatchToProps = (dispatch) => {
 const styles = StyleSheet.create({
   emptyContainer: {
     height: height,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   bottomContainer: {
-    flexDirection: 'row',
-    position: 'absolute',
+    flexDirection: "row",
+    position: "absolute",
     bottom: 0,
     left: 0,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     elevation: 20,
   },
   price: {
     fontSize: 18,
     margin: 20,
-    color: 'red',
+    color: "red",
   },
   hiddenContainer: {
     flex: 1,
-    justifyContent: 'flex-end',
-    flexDirection: 'row',
+    justifyContent: "flex-end",
+    flexDirection: "row",
   },
   hiddenButton: {
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'flex-end',
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "flex-end",
     paddingRight: 25,
     height: 70,
     width: width / 1.2,
